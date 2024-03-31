0e22f128029054373492df3bf58ac91688cfad0a
Isac Paz <isacgomesp@gmail.com>
2024-03-31 14:35:15 -0300

    feat: home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,21 @@ export default function Home() {
 
   return (
     <div className="bg-gray-900 text-white min-h-screen">
-      {user?.name}
+      <div className="max-w-5xl mx-auto p-6 ">
+        {user ? (
+          <div>
+            Olá,
+            <br />
+            <span className="font-medium text-xl">
+              {user?.name}
+            </span>
+          </div>
+        ) : (
+          <div>
+            <span>Faça login para acessar sua conta.</span>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
